7114b33a97f7a6ce34a02c4e71aa960202c2e1cc
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-08-19 16:24:15 +0100

    created useState Hook

--- gatsby-project/src/components/Header/Header.js
+++ gatsby-project/src/components/Header/Header.js
@@ -1,24 +1,44 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import "./Header.css"
 import logo from "../../images/logo_b.png"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="header">
-      <div className="headerGroup">
-        <Link className="logo" to="/">
-          <img src={logo} />
-        </Link>
-        <Link to="/">About</Link>
-        <Link to="/">Work</Link>
-        <Link to="/">Services</Link>
-        <Link to="/">
-          <button>Contact Me</button>
-        </Link>
+const Header = () => {
+  const [hasScroll, setHasScroll] = useState(false)
+
+  const handleScroll = event => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 500) {
+      setHasScroll(true)
+    } else {
+      setHasScroll(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <header>
+      <div className={hasScroll ? "header headerScroll" : "header"}>
+        <div className="headerGroup">
+          <Link className="logo" to="/">
+            <img src={logo} />
+          </Link>
+          <Link to="/">About</Link>
+          <Link to="/">Work</Link>
+          <Link to="/">Services</Link>
+          <Link to="/">
+            <button>Contact Me</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
+
+// useState
--- gatsby-project/src/components/Hero/Hero.js
+++ gatsby-project/src/components/Hero/Hero.js
@@ -13,7 +13,8 @@ const Hero = () => {
       <div className="hero">
         <div className="heroGroup">
           <h1>
-            Hey there, <br /> I'm Bav Kudhail
+            Hey there, <br /> I'm
+            <span className="name"> Bav Kudhail</span>
           </h1>
           <p>I build stunning web applications</p>
           <div className="techStack">
@@ -35,7 +36,7 @@ const Hero = () => {
                 repeatCount={"indefinite"}
                 fill="freeze"
                 attributeName="d"
-                dur={"30s"}
+                dur={"20s"}
                 values="M4458 576L0.000174522 576L0.000196892 320.116C0.000196892 320.116 1336.11 488.952 2440.92 123.492C3545.73 -241.967 4458 320.116 4458 320.116L4458 576Z;
                 M4458 545L0.000174522 545L0.000196905 288.973C0.000196905 288.973 966.546 -197.844 2440.92 92.2395C3915.29 382.323 4458 288.973 4458 288.973L4458 545Z;
                 M4458 408L0.000174522 408L0.000196923 151.765C0.000196923 151.765 1192.68 265.361 2413.78 69.7912C3634.89 -125.779 4458 151.765 4458 151.765L4458 408Z;
